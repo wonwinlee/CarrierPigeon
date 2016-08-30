1ab48f5abde772fe1393bbd8620f99aebba00d70
wonwinlee <wonwinry@163.com>
2016-08-30 11:37:03 +0800

    项目初始化

---
+++ CarrierPigeon/CarrierPigeon/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CarrierPigeon
+//
+//  Created by hudongshili on 16/8/30.
+//  Copyright © 2016年 wenli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CarrierPigeon/CarrierPigeon/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CarrierPigeon
+//
+//  Created by hudongshili on 16/8/30.
+//  Copyright © 2016年 wenli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
